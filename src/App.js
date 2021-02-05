37137e12db084a2688637ef6af02d2951d406c59
Kuldeep Patel <patelkuldeep0001@gmail.com>
2021-02-05 21:42:54 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+// import DarkMode from "./components/darkMode/DarkMode"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>KULDEEP PATEL</h1>
+        <h3>WEB DEVELOPER | ELECTRONIC AND TELECOMMUNICATION ENGINEERING | ROBOTICS ENTHUSIASTIC</h3>
+
       </header>
     </div>
+    // <div className="App">
+    //   <nav>
+    //     <a href="/">Home</a>
+    //     <a href="/">Projects</a>
+    //     <a href="/">About</a>
+    //     <a href="/">Contact</a>
+    //     <DarkMode />
+    //   </nav>
+    //   <h1>Hello World</h1>
+    //   <div id="image"></div>
+    //   <p>
+    //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget
+    //     scelerisque neque, quis scelerisque erat. Quisque venenatis molestie
+    //     sapien, dapibus viverra nulla hendrerit eget. Pellentesque egestas
+    //     ultrices accumsan. Ut ac magna vel ex maximus ultricies. Nulla facilisi.
+    //     Suspendisse gravida sem eu odio mattis ullamcorper. Curabitur feugiat
+    //     ipsum vel vulputate ultricies.
+    //   </p>
+    //   <p>
+    //     Praesent pulvinar faucibus risus in iaculis. Sed erat felis, pretium sit
+    //     amet ultricies non, porta et lacus. Curabitur a urna mi. Sed eleifend
+    //     sed erat eget viverra. Quisque sit amet purus viverra massa posuere
+    //     congue. Suspendisse efficitur venenatis enim, id hendrerit enim ultrices
+    //     sed. Nam sed dapibus nisi.
+    //   </p>
+    // </div>
   );
 }
 
