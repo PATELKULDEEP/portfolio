0337ede76e0dd5c328856ca09add8a9da0ca1868
Kuldeep Patel <patelkuldeep0001@gmail.com>
2021-05-24 11:20:50 +0530

    projects added

--- src/components/ProjectsComponent/FullProjects.jsx
+++ src/components/ProjectsComponent/FullProjects.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import Header from '../HeaderComponent/Header';
 import './fullProjectsStyle.css';
-import kpimg from "../../assets/images/kp.png";
+import kpImg from "../../assets/images/kp.png";
+import mitImg from '../../assets/images/mit.png';
+import sihImg from '../../assets/images/sih.png';
+import technohubImg from '../../assets/images/technohub.png';
+import tedxImg from '../../assets/images/tedx.png';
+import todoImg from '../../assets/images/todo.png';
+import zenshinImg from '../../assets/images/zenshin.png';
+
 
 function FullProjects() {
     return (
@@ -15,13 +22,73 @@ function FullProjects() {
         </div>
         
     <div className='fullprojects-cards'>
-      <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
+    <div className='card'>
+          <img src={tedxImg} alt='Avatar'  />
           <div className='container'>
             <h4>
               <b>TEDxBITD</b>
             </h4>
             <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
+            <div className="card-sources">
+                <div className="card-sources-link">
+
+                <a href="" >Project</a>
+                </div>
+                {/* <div className="card-sources-link">
+                    
+                    <a href="" >Code</a>
+                    </div> */}
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <img src={technohubImg} alt='Avatar'  />
+          <div className='container'>
+            <h4>
+              <b>TechnoHub</b>
+            </h4>
+            <p>Built the website for one of the official technical clubs of my
+college, used by over 1000 peoples
+Built the entire backend using firebase and firestore and
+Oauth for authentication</p>
+            <div className="card-sources">
+                <div className="card-sources-link">
+
+                <a href="" >Project</a>
+                </div>
+
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <img src={sihImg} alt='Avatar'  />
+          <div className='container'>
+            <h4>
+              <b>SIH Hackathon</b>
+            </h4>
+            <p>Android application and web portal for police for effective
+criminal tracking and record maintenance
+Used Firebase a back-end for entire application</p>
+            <div className="card-sources">
+                <div className="card-sources-link">
+
+                <a href="https://drive.google.com/file/d/1ltE_6_12S5zY99vIHuCPhYs6-Q61bwE5/view" >Presentation</a>
+                </div>
+                <div className="card-sources-link">
+                    
+                    <a href="" >Code</a>
+                    </div>
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <img src={zenshinImg} alt='Avatar'  />
+          <div className='container'>
+            <h4>
+              <b>Zenshin</b>
+            </h4>
+            <p>Developed a web app that connects service providers with their customers through an easy-to-use interface. And won the Best
+Resource for Businesses Prize.</p>
             <div className="card-sources">
                 <div className="card-sources-link">
 
@@ -35,12 +102,13 @@ function FullProjects() {
           </div>
         </div>
         <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
+          <img src={mitImg} alt='Avatar'  />
           <div className='container'>
             <h4>
-              <b>TEDxBITD</b>
+              <b>MIT Covid-19 Challenge</b>
             </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
+            <p>We developed a platform to support the facility allocations in resp. hospitals. It helped to improve patient flow and provide reliable and
+trustworthy information about the availability of nearest centres</p>
             <div className="card-sources">
                 <div className="card-sources-link">
 
@@ -54,130 +122,16 @@ function FullProjects() {
           </div>
         </div>
         <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
+          <img src={todoImg} alt='Avatar'  />
           <div className='container'>
             <h4>
-              <b>TEDxBITD</b>
+              <b>TODO Website </b>
             </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
+            <p>Buit using ReactJS, a full CRUD based functionality</p>
             <div className="card-sources">
                 <div className="card-sources-link">
 
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
+                <a href="https://dreamy-feynman-0b9ee7.netlify.app/" target="_blank" >Project</a>
                 </div>
                 <div className="card-sources-link">
                     
--- src/components/ProjectsComponent/Projects.jsx
+++ src/components/ProjectsComponent/Projects.jsx
@@ -2,20 +2,87 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../HeaderComponent/Header";
 import "./projectsStyle.css";
-import kpimg from "../../assets/images/kp.png";
+import kpImg from "../../assets/images/kp.png";
+import mitImg from '../../assets/images/mit.png';
+import sihImg from '../../assets/images/sih.png';
+import technohubImg from '../../assets/images/technohub.png';
+import tedxImg from '../../assets/images/tedx.png';
+import todoImg from '../../assets/images/todo.png';
+import zenshinImg from '../../assets/images/zenshin.png';
+
 
 function Projects() {
   return (
     <div className='projects'>
-      <h1>Projects</h1>
+      <h1>Work / Projects</h1>
       <div className='projects-cards'>
       <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
+          <img src={tedxImg} alt='Avatar'  />
           <div className='container'>
             <h4>
               <b>TEDxBITD</b>
             </h4>
             <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
+            <div className="card-sources">
+                <div className="card-sources-link">
+
+                <a href="" >Project</a>
+                </div>
+                {/* <div className="card-sources-link">
+                    
+                    <a href="" >Code</a>
+                    </div> */}
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <img src={technohubImg} alt='Avatar'  />
+          <div className='container'>
+            <h4>
+              <b>TechnoHub</b>
+            </h4>
+            <p>Built the website for one of the official technical clubs of my
+college, used by over 1000 peoples
+Built the entire backend using firebase and firestore and
+Oauth for authentication</p>
+            <div className="card-sources">
+                <div className="card-sources-link">
+
+                <a href="" >Project</a>
+                </div>
+
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <img src={sihImg} alt='Avatar'  />
+          <div className='container'>
+            <h4>
+              <b>SIH Hackathon</b>
+            </h4>
+            <p>Android application and web portal for police for effective
+criminal tracking and record maintenance
+Used Firebase a back-end for entire application</p>
+            <div className="card-sources">
+                <div className="card-sources-link">
+
+                <a href="https://drive.google.com/file/d/1ltE_6_12S5zY99vIHuCPhYs6-Q61bwE5/view" >Presentation</a>
+                </div>
+                <div className="card-sources-link">
+                    
+                    <a href="" >Code</a>
+                    </div>
+            </div>
+          </div>
+        </div>
+        <div className='card'>
+          <img src={zenshinImg} alt='Avatar'  />
+          <div className='container'>
+            <h4>
+              <b>Zenshin</b>
+            </h4>
+            <p>Developed a web app that connects service providers with their customers through an easy-to-use interface. And won the Best
+Resource for Businesses Prize.</p>
             <div className="card-sources">
                 <div className="card-sources-link">
 
@@ -29,12 +96,13 @@ function Projects() {
           </div>
         </div>
         <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
+          <img src={mitImg} alt='Avatar'  />
           <div className='container'>
             <h4>
-              <b>TEDxBITD</b>
+              <b>MIT Covid-19 Challenge</b>
             </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
+            <p>We developed a platform to support the facility allocations in resp. hospitals. It helped to improve patient flow and provide reliable and
+trustworthy information about the availability of nearest centres</p>
             <div className="card-sources">
                 <div className="card-sources-link">
 
@@ -48,16 +116,16 @@ function Projects() {
           </div>
         </div>
         <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
+          <img src={todoImg} alt='Avatar'  />
           <div className='container'>
             <h4>
-              <b>TEDxBITD</b>
+              <b>TODO Website </b>
             </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
+            <p>Buit using ReactJS, a full CRUD based functionality</p>
             <div className="card-sources">
                 <div className="card-sources-link">
 
-                <a href="" >Project</a>
+                <a href="https://dreamy-feynman-0b9ee7.netlify.app/" target="_blank" >Project</a>
                 </div>
                 <div className="card-sources-link">
                     
@@ -66,120 +134,7 @@ function Projects() {
             </div>
           </div>
         </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
-        <div className='card'>
-          <img src={kpimg} alt='Avatar'  />
-          <div className='container'>
-            <h4>
-              <b>TEDxBITD</b>
-            </h4>
-            <p>Developed a ReactJS based website for the TEDxBITD event in our college, used by over 500 users Worked on the backend using Firebase and Firestore</p>
-            <div className="card-sources">
-                <div className="card-sources-link">
-
-                <a href="" >Project</a>
-                </div>
-                <div className="card-sources-link">
-                    
-                    <a href="" >Code</a>
-                    </div>
-            </div>
-          </div>
-        </div>
+        
 
 </div>
       <Link
